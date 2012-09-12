9433280b648f29797ed344efc0c24b224378cc35
Gowtham Narisipalli <narisipalli@gmail.com>
2012-09-11 23:04:41 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsCodelab::Application.config.secret_token = '26031393dfeefc06584b6e25dbb249052967275aaf99c120f86c129c46c9a51176ec71e1bcb331d50a30743d4349fad410eb568963878f747adda0a2b2885498'
